e363fcbd38a813f9bd36bea4cfb1bca1c66144d8
Kai Lu <lukai@zufe.edu.cn>
2018-05-10 10:17:57 +0800

    Create 959A.c

---
+++ 959A.c
@@ -0,0 +1,13 @@
+/*Problem 959A Mahmoud and Ehab and the even-odd game*/
+#include <stdio.h>
+int main()
+{
+    int n;
+
+    scanf("%d", &n);
+    if (n % 2 == 0)
+        printf("Mahmoud");
+    else
+        printf("Ehab");
+    return 0;
+}
