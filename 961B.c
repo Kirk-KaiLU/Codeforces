cd49aa5cb303a619d30b1c8d25f92c2f79c0452b
Kai Lu <kai.lu.academic@gmail.com>
2018-05-10 19:39:34 +0800

    Update 961B.c

--- 961B.c
+++ 961B.c
@@ -1,38 +1,38 @@
-/*Problem 961B Lecture Sleep*//*Overtime on test5*/
+/*Problem 961B Lecture Sleep*/ /*Time limit exceeded on test5*/
 #include <stdio.h>
 int main()
 {
-    long n, k, sum, awake, temp = 0;
-    int i, j, a[100000] = {}, t[100000] = {};
+    long long n, k, sum = 0, awake, temp = 0, i, j;
+    int a[100000] = {}, t[100000] = {};
     /* n is the duration of the lecture in minutes 
      * k is the number of the minutes you can keep her awake
      * sum is the number of theorems she could write down herself
      * awake is the number of theorems she could write down by being awake
      */
-    scanf("%ld %ld", &n, &k);
+    scanf("%lld %lld", &n, &k);
     for (i = 0; i < n; i++)
         scanf("%d", &a[i]);
     for (i = 0; i < n; i++)
+    {
         scanf("%d", &t[i]);
-    for (i = 0; i < n; i++)
-        sum += a[i] * t[i];
+        if (t[i] == 1)
+            sum += a[i];
+    }
     for (i = 0; i < n; i++)
         if (t[i] == 1)
-            t[i] = 0;
-        else
-            t[i] = 1;
+            a[i] = 0;
 
     for (i = 0; i < n - k + 1; i++)
     {
         awake = 0;
         for (j = i; j < i + k; j++)
-            awake += a[j] * t[j];
+            awake += a[j];
         if (awake > temp)
             temp = awake;
     }
 
     sum = sum + temp;
-    printf("%ld", sum);
+    printf("%lld", sum);
 
     return 0;
 }
